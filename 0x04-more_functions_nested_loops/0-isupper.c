5e9ea11a0536473be46cce399bc4f41f8751d71b
Allan Munene <allanmunene200@gmail.com>
2022-09-24 08:29:36 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,9 @@
 #include "munene.h"
-/**
-*
-*  * _isupper - checks if parameter is an uppercase character.
-*
-*   * @c: input character.
-*
-*    * Return: 1 if is an uppercase character, 0 in other case.
-*
-*     */
+/*
+*_isupper - checks if parameter is an uppercase character.
+*@c: input character.
+*Return: 1 if is an uppercase character, 0 in other case.
+*/
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
